6d7b499d4ff56119b6d07fd9e5a5dcbaae06b96e
reysilvaa <reynaldsilva123@gmail.com>
2025-02-22 02:19:50 +0700

    .

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,6 @@ import WorkExperience from './components/WorkExperience';
 import Education from './components/Education';
 import Projects from './components/Projects';
 import Skills from './components/Skills';
-import Services from './components/Services';
 import Testimonials from './components/Testimonials';
 import Contact from './components/Contact';
 
